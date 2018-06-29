2ca076f4dddb9b4bb4b82fff527c2a1c5aedbe30
buck-nicholas <buck.nicholas.c@gmail.com>
2018-06-29 11:09:50 -0500

    Added inventory loss parameters for sugar and lemons

--- LemonaidStand/Inventory.cs
+++ LemonaidStand/Inventory.cs
@@ -12,12 +12,19 @@ namespace LemonaidStand
         public int sugarAmount;
         public int iceAmount;
         public int cupAmount;
+        private int spoilAmount;
+        private static readonly Random randomInt = new Random();
         public Inventory()
         {
             lemonAmount = 0;
             sugarAmount = 0;
             iceAmount = 0;
             cupAmount = 0;
+        }
+        public int RandomNumberGenerater(int min, int max)
+        {
+            int RandomNumber = randomInt.Next(min, max);
+            return RandomNumber;
         }
         public void DisplayInventory()
         {
@@ -29,6 +36,26 @@ namespace LemonaidStand
             {
                 iceAmount = 0;
                 Console.WriteLine("Your ice has melted...");
+            }
+            if (lemonAmount > 0 && RandomNumberGenerater(0, 3) == 2)
+            {
+                spoilAmount = lemonAmount / 2;
+                if (spoilAmount > 10)
+                {
+                    spoilAmount = 10;
+                }
+                lemonAmount -= spoilAmount;
+                Console.WriteLine($"Some of your lemons have spoiled! -{spoilAmount} Lemon(s)");
+            }
+            if (sugarAmount > 0 && RandomNumberGenerater(0, 3) == 2)
+            {
+                spoilAmount = sugarAmount / 2;
+                sugarAmount -= spoilAmount;
+                if (spoilAmount > 10)
+                {
+                    spoilAmount /= 3;
+                }
+                Console.WriteLine($"You've got ants! -{spoilAmount} cup(s) of sugar");
             }
         }
     }
